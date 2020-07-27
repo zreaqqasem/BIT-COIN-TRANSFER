cb6c1274279fbc915a57a12118a87c09d0cb3019
qasem <zreaqqasem@ymail.com>
2020-07-28 00:53:18 +0300

    my second practice app working with api

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Qasem Zreaq on 01/07/2020.
+//  Copyright © 2020 Qasem Zreaq. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
